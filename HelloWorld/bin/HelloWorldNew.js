e781f2226c8518902697ad9224920962be816980
Rajesh <mailrbn@gmail.com>
2018-04-02 16:21:04 +1000

    ExpressJS Crash Course
    Travesy Media

---
+++ HelloWorld/bin/HelloWorldNew.js
@@ -0,0 +1,147 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+
+var db = mongojs('customerapp', ['users']);
+var ObjectId = mongojs.ObjectId;
+var app = express();
+
+/*var logger = function (req, res, next) {
+    console.log("Logging...");
+    next();
+}
+
+app.use(logger);*/
+
+// Global vars
+app.use(function(req, res, next) {
+    res.locals.errors = null;
+    next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({extended: false}));
+
+// Set static path
+//app.use(express.static(path.join(__dirname, 'public')));
+
+/*var person = [
+    {
+        name:'Jeff',
+        age: 30
+    },
+    {
+        name:'Bill',
+        age: 40
+    },
+    {
+        name:'Sam',
+        age: 39
+    }
+]*/
+
+/*var users = [
+    {
+        id: 1,
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'johndoe@gmail.com'
+    },
+    {
+        id: 2,
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bobsmith@gmail.com'
+    },
+    {
+        id: 3,
+        first_name: 'Jill',
+        last_name: 'Jackson',
+        email: 'jjackson@gmail.com'
+    }
+]*/
+
+
+app.get('/', function (req, res) {
+    //res.send("Hello, Rajesh!")
+    //res.json(person);
+    // find everything
+    db.users.find(function (err, docs) {
+        // docs is an array of all the documents in mycollection
+        console.log(docs);
+        res.render('index', {
+            title: 'Customer',
+            users: docs
+    });
+
+    });
+});
+
+app.post('/users/add', function (req, res) {
+    //console.log('FORM SUBMITTED')
+    req.checkBody('first_name', 'first name is required').notEmpty();
+    req.checkBody('last_name', 'last name is required').notEmpty();
+    req.checkBody('email', 'email is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('index', {
+            title: 'Customer',
+            users: users,
+            errors: errors
+        });
+    }   else {
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+        }
+
+        db.users.insert(newUser, function(err, result) {
+            if (err) {
+                console.log(err);
+            }
+            res.redirect('/');
+        });
+    }
+});
+
+app.delete('/users/delete/:id', function (req, res) {
+    //console.log(req.params.id);
+    db.users.remove({_id: ObjectId(req.params.id)}, function (err, result) {
+        if(err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
